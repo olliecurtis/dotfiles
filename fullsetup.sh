9c55a288f20ce7f855a505f4d2651cba914de790
Ollie Curtis <8831547+olliecurtis@users.noreply.github.com>
2021-05-23 19:09:57 +0100

    Updating ohmyzsh url

--- fullsetup.sh
+++ fullsetup.sh
@@ -14,7 +14,7 @@ chmod +x ./brewfiles/brewfile.sh
 
 # Prettify Terminal
 # Install Oh My Zsh
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Install Powerline themes
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
